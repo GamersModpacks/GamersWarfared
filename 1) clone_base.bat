3cc5138eb3dd0983c68c28bf0f9ad2b6d1869ca4
Dmytro Denchenko <dima.den4enko.ok@gmail.com>
2023-10-31 17:58:17 +0100

    Beta 1.7

--- 1) clone_base.bat
+++ 1) clone_base.bat
@@ -1,5 +1,5 @@
 @echo off
-git clone https://github.com/Den4enko/ForgePackBase base
+git clone https://github.com/Den4enko/PackFramework base
 cd base
-git checkout 1.19.2
+git checkout forge/1.19.2
 @pause
--- 3) build.bat
+++ 3) build.bat
@@ -1,10 +1,17 @@
 @echo off
+:: Cleaning old files.
 rmdir beta /s /q
+:: Merging two modpacks
 xcopy base\*.* beta\ /e
 xcopy mod\*.* beta\ /e /y
+:: Removing unused files.
 del beta\README.md
 del beta\.gitattributes
 del beta\.packwizignore
+:: Pack Additions
 cd beta
+packwiz refresh
+packwiz remove xaeros-minimap
+packwiz cf install xaeros-minimap-fair-play-edition
 packwiz refresh --build
 @pause
